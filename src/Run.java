2ea3bca3850eb245564e52c8999bd06026419af4
haaa20 <hugoaaabbott@sussex.ac.uk>
2022-04-07 17:46:01 +0100

    Created Run class. This means AddressBook is instanced before running

---
+++ src/Run.java
@@ -0,0 +1,8 @@
+import javafx.application.Application;
+
+public class Run {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.run(args);
+    }
+}
